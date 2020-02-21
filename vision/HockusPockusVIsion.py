0e62e36b4542b3d045e10a1b84f8415bf942585e
FrancisCardinal <francis.cardinal.01@gmail.com>
2020-02-21 08:22:15 -0500

    fixed : getNextFrame() of CameraROS class shouldn't grab the same frame twice anymore

--- vision/HockusPockusVIsion.py
+++ vision/HockusPockusVIsion.py
@@ -7,6 +7,7 @@ import sys
 import pickle
 import os
 import copy
+import threading
 
 try:
     import rospy
@@ -116,23 +117,36 @@ class Camera(ABC) :
         pass
 
 class CameraROS(Camera) :
-    def __init__(self):
+    def __init__(self,i_FPS):
         super().__init__()
         rospy.init_node('vision')
         self.m_webcam = rospy.Subscriber("/usb_cam/image_raw", Image, updateFrame)
         self.m_frame = None
         self.m_bridge = CvBridge()
+        timeBetweenFrames = 1000 / i_FPS  # The time between two frames is : 1000 ms/s * (1s/Number of frames per second)
+        self.waitTime = int(timeBetweenFrames/4)
+        self.hasNewFrame = False
+        self.hasNewFrameLock = threading.Lock()
 
     def updateFrame(self,i_image):
         try:
             cv_image = bridge.imgmsg_to_cv2(i_image, "passthrough")
+            self.m_frame = cv_image
+            self.__setHasNewFrame(True)
         except CvBridgeError:
             pass
 
-        self.m_frame = cv_image
-
     def getNextFrame(self):
+        while (not self.hasNewFrame) :
+            cv2.waitKey(int(self.waitTime)) & 0xFF
+            
+        self.__setHasNewFrame(False)
         return (True,self.m_frame)
+
+    def __setHasNewFrame(self,i_hasNewFrame):
+        self.hasNewFrameLock.acquire()
+        self.hasNewFrame = i_hasNewFrame
+        self.hasNewFrameLock.release()
 
 class CameraUSB(Camera) :
     def __init__(self, i_videoCaptureIndex=0, i_FPS=30):
