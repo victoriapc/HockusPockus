2a0248ba902c265ecd2214a34169f4c5eb4d750e
FrancisCardinal <francis.cardinal.01@gmail.com>
2020-02-25 18:34:34 -0500

    Moved a switch case that was in the main into the Builder, as this switch case should be part of this class's responsibility

--- vision/src/PuckDetectorBuilder.py
+++ vision/src/PuckDetectorBuilder.py
@@ -1,3 +1,11 @@
+from CameraROS import  CameraROS
+from CameraUSB import  CameraUSB
+
+try :
+    import rospy
+except :
+    pass
+
 from PuckDetectorROS import PuckDetectorROS
 from PuckDetectorUSB import PuckDetectorUSB
 from PuckDetectorConfiguration import PuckDetectorConfiguration
@@ -13,17 +21,29 @@ class PuckDetectorBuilder(object):
     ROS = 0
     USB = 1
 
-    def __init__(self, i_camera, i_mode,i_path, i_reconfigure):
+    def __init__(self, i_mode,i_FPS):
         """
         PuckDetectorBuilder class's constructor. Initializes, notably, self.m_camera a
         pointer to a concrete implementation of the abstract base class Camera
         Args:
-            i_camera: pointer to a concrete implementation of the abstract base class Camera
+            i_mode: Indicates the type of the camera that is going to be used (ROS,USB,...)
+            i_FPS : The number of frame per seconds of the camera
         """
-        self.m_camera = i_camera
+        self.m_camera = None
         self.m_mode = i_mode
-        self.m_path = i_path + '/config.json'
-        self.m_reconfigure = i_reconfigure
+        self.m_path = ""
+        self.m_reconfigure = False
+
+        if self.m_mode == PuckDetectorBuilder.ROS:
+            self.m_camera = CameraROS(i_FPS)
+            self.m_path = rospy.get_param('/vision/config_folder')
+            self.m_reconfigure = rospy.get_param('/vision/reconfigure')
+
+        elif self.m_mode == PuckDetectorBuilder.USB:
+            self.m_camera = CameraUSB(0, i_FPS)
+            self.m_path = os.getcwd()
+
+        self.m_path = self.m_path + '/config.json'
 
     def build(self):
         """
