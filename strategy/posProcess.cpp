b6578eaa4a612de0c0592f8aa684d76938fdb4f3
EDesbiens <etiennedesbiens@hotmail.com>
2020-03-30 23:05:01 -0400

    fixed pos process bugs

--- strategy/posProcess.cpp
+++ strategy/posProcess.cpp
@@ -7,17 +7,25 @@ const int LEFT = -1;
 const int RIGHT = 1;
 const int maxIter = 50;
 
+ros::Subscriber _subscriberDimensionsTerrain;
+ros::Subscriber _subscriberPositionActuellePuck;
+ros::Publisher  _publisherPositionDesiree;
+
 struct Vector2
 {
+	Vector2():X(0),Y(0) {}
+	Vector2(float i_x, float i_y):X(i_x),Y(i_y) {}
 	float X;
 	float Y;
 };
 
 struct Vector3
 {
+	Vector3():pos(0, 0),iter(0){}
+	Vector3(Vector2 i_pos, int i_iter):pos(i_pos.X, i_pos.Y),iter(i_iter){}
 	Vector2 pos;
 	int iter;
-}
+};
 
 struct Terrain
 {
@@ -42,14 +50,14 @@ void initTerrain(const std_msgs::Float32MultiArray i_dimensionsCotes)
 	map.BasGauche.X = 0 ; 
 	map.BasGauche.Y = 0 ; 
 	
-	map.HautGauche.X = BasGauche.X; 
-	map.HautGauche.Y = i_dimensionsCotes[0];
+	map.HautGauche.X = map.BasGauche.X; 
+	map.HautGauche.Y = i_dimensionsCotes.data[0];
 	
-	map.HautDroite.X = i_dimensionsCotes[1];
-	map.HautDroite.Y = HautGauche.Y
+	map.HautDroite.X = i_dimensionsCotes.data[1];
+	map.HautDroite.Y = map.HautGauche.Y;
 	
-	map.BasDroite.X = HautDroite.X;
-	map.BasDroite.Y = BasGauche.Y;
+	map.BasDroite.X = map.HautDroite.X;
+	map.BasDroite.Y = map.BasGauche.Y;
 	
 	map.butHaut.X = (map.HautDroite.Y-map.HautGauche.Y)/(map.HautDroite.X-map.HautGauche.X);
 	map.butHaut.Y = map.HautDroite.Y - (map.butHaut.X * map.HautDroite.X);
@@ -82,11 +90,86 @@ void predictionToDesiredPosition(Vector2 i_predictedPosition)
 	_publisherPositionDesiree.publish(msg); 
 }
 
+
+Vector3 linePredict(float A, float B, int dir, Vector2 pos, int iteration)
+{
+	iteration++;
+	
+	
+	float distBut = 1000;
+	float distBut2 = 1000;
+	float distMur = 1000;
+	float distMur2 = 1000;
+	
+	//But robot
+	float tempXBut = (B-map.butHaut.Y)/(map.butHaut.X-A);
+	float tempYBut = map.butHaut.X * tempXBut + map.butHaut.Y;
+	
+	if((map.HautGauche.X < tempXBut) && (tempXBut < map.HautDroite.X) && (dir*A>0))
+	{
+		distBut = sqrt(pow(tempXBut-pos.X, 2) + pow(tempYBut-pos.Y,2));
+	}
+	
+	//But joueur
+	float tempXBut2 = (B-map.butBas.Y)/(map.butBas.X-A);
+	float tempYBut2 = map.butBas.X * tempXBut2 + map.butBas.Y;
+	
+	if((map.BasGauche.X < tempXBut2) && (tempXBut2 < map.BasDroite.X) && (dir*A<0))
+	{
+		distBut2 = sqrt(pow(tempXBut2-pos.X, 2) + pow(tempYBut2-pos.Y,2));
+	}
+	
+	
+	//Mur gauche
+	float tempXMur = (B-map.murGauche.Y)/(map.murGauche.X-A);
+	float tempYMur = map.murGauche.X * tempXMur + map.murGauche.Y;
+	
+	if((tempYMur > map.BasGauche.Y) && (tempYMur < map.HautGauche.Y) && (dir < 0))
+	{
+		distMur = sqrt(pow(tempXMur - pos.X,2)+pow(tempYMur-pos.Y,2));
+	}
+	
+	//Mur droite
+	float tempXMur2 = (B-map.murDroite.Y)/(map.murDroite.X-A);
+	float tempYMur2 = map.murDroite.X * tempXMur2 + map.murDroite.Y;
+	
+	if((tempYMur2 > map.BasDroite.Y) && (tempYMur < map.HautDroite.Y) && (dir > 0))
+	{
+		distMur2 = sqrt(pow(tempXMur2 - pos.X,2)+pow(tempYMur2-pos.Y,2));
+	}
+	
+	if(distBut < distBut2 && distBut < distMur && distBut < distMur2 && iteration < maxIter)
+		return Vector3(Vector2(tempXBut, tempYBut), iteration);
+	else if(distBut2 < distBut && distBut2 < distMur && distBut2 < distMur2 && iteration < maxIter)
+	{
+		float A2 = A*-1;
+		float B2 = tempYBut2-A2*tempXBut2;
+		return linePredict(A2,B2,dir, Vector2(tempXBut2, tempYBut2), iteration);
+	}
+	else if(distMur < distBut && distMur < distBut2 && distMur < distMur2 && iteration < maxIter)
+	{
+		float A2 = A*-1;
+		float B2 = tempYMur-A2*tempXMur;
+		int dir2 = dir*-1;
+		return linePredict(A2,B2,dir2, Vector2(tempXMur,tempYMur),iteration);
+	}
+	else if(distMur2 < distBut && distMur2 < distBut2 && distMur2 < distMur && iteration < maxIter)
+	{
+		float A2 = A*-1;
+		float B2 = tempYMur2-A2*tempXMur2;
+		int dir2 = dir*-1;
+		return linePredict(A2,B2,dir2, Vector2(tempXMur2,tempYMur2),iteration);
+	}
+	else
+		return Vector3(Vector2(1000, 1000), iteration);
+}
+
+
 //Fonction réception de données
 void reception(const geometry_msgs::Point i_puckPos)
 {
-	pos.X i_puckPos.x ; 
-	pos.Y i_puckPos.y ; 
+	pos.X = i_puckPos.x ; 
+	pos.Y = i_puckPos.y ; 
 
 	float A = (pos.Y - posPrec.Y)/(pos.X-posPrec.X);
 	float B = pos.Y - A * pos.X;
@@ -107,85 +190,6 @@ void reception(const geometry_msgs::Point i_puckPos)
 	predictionToDesiredPosition(prediction.pos);
 	posPrec = pos;
 }
-
-Vector3 linePredict(float A, float B, int dir, Vector2 pos, int iteration)
-{
-	iteration++;
-	
-	Vector2 posWallHit = new Vector2(0,0);
-	
-	float distBut = 1000;
-	float distBut2 = 1000;
-	float distMur = 1000;
-	float distMur2 = 1000;
-	
-	//But robot
-	float tempXBut = (B-map.butHaut.Y)/(map.butHaut.X-A);
-	float tempYBut = map.butHaut.X * tempXBut + map.butHaut.Y;
-	
-	if((map.HautGauche.X < tempXBut) && tempXBut < map.HautDroite.X) && dir*A>0)
-	{
-		distBut = sqrt(pow(tempXBut-pos.X, 2) + pow(tempYBut-pos.Y,2));
-	}
-	
-	//But joueur
-	float tempXBut2 = (B-map.butBas.Y)/(map.butBas.X-A);
-	float tempYBut2 = map.butBas.X * tempXBut2 + map.butBas.Y;
-	
-	if((map.BasGauche < tempXBut2) && tempXBut2 < map.BasDroite) && dir*A<0)
-	{
-		distBut2 = sqrt(pow(tempXBut2-pos.X, 2) + pow(tempYBut2-pos.Y,2));
-	}
-	
-	
-	//Mur gauche
-	float tempXMur = (B-map.murGauche.Y)/(map.murGauche.X-A);
-	float tempYMur = map.murGauche.X * tempXMur + map.murGauche.Y;
-	
-	if(tempYMur > map.BasGauche.Y && tempYMur < map.HautGauche.Y && dir < 0)
-	{
-		distMur = sqrt(pow(tempXMur - pos.X,2)+pow(tempYMur-pos.Y,2));
-	}
-	
-	//Mur droite
-	float tempXMur2 = (B-map.murDroite.Y)/(map.murDroite.X-A);
-	float tempYMur2 = map.murDroite.X * tempXMur2 + map.murDroite.Y;
-	
-	if(tempYMur2 > map.BasDroite.Y && tempYMur1 < map.HautDroite.Y && dir > 0)
-	{
-		distMur2 = sqrt(pow(tempXMur2 - pos.X,2)+pow(tempYMur2-pos.Y,2));
-	}
-	
-	if(distBut < distBut2 && distBut < distMur && distBut < distMur2 && iteration < maxIter)
-		return new Vector3(new Vector2(tempXBut, tempYBut), iteration);
-	else if(distBut2 < distBut && distBut2 < distMur && distBut2 < distMur2 && iteration < maxIter)
-	{
-		float A2 = A*-1;
-		float B2 = tempYBut2-A2*tempXBut2;
-		return linePredict(A2,B2,dir,new Vector2(tempXBut2, tempYBut2), iteration);
-	}
-	else if(distMur < distBut && distMur < distBut2 && distMur < distMur2 && iteration < maxIter)
-	{
-		float A2 = A*-1;
-		float B2 = tempYMur-A2*tempXMur;
-		int dir2 = dir*-1;
-		return linePredict(A2,B2,dir2,new Vector2(tempXMur,tempYMur),iteration);
-	}
-	else if(distMur2 < distBut && distMur2 < distBut2 && distMur2 < distMur && iteration < maxIter)
-	{
-		float A2 = A*-1;
-		float B2 = tempYMur2-A2*tempXMur2;
-		int dir2 = dir*-1;
-		return linePredict(A2,B2,dir2,new Vector2(tempXMur2,tempYMur2),iteration);
-	}
-	else
-		return new Vector3(new Vector2(1000, 1000), iteration);
-}
-
-
-ros::Subscriber _subscriberDimensionsTerrain;
-ros::Subscriber _subscriberPositionActuellePuck;
-ros::Publisher  _publisherPositionDesiree;
 
 int main(int argc, char*argv[])
 {
