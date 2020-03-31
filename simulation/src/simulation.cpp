e79a145ff742530ddc00969923d329ee79b5f8c5
FrancisCardinal <francis.cardinal.01@gmail.com>
2020-03-31 17:23:15 -0400

    Added a simulation package to simulate the output of the vision package

---
+++ simulation/src/simulation.cpp
@@ -0,0 +1,34 @@
+#include "ros/ros.h"
+#include <geometry_msgs/Point.h>
+#include <chrono>
+#include <thread>
+#include <unistd.h>
+
+// Pub and Sub
+ros::Publisher puck_pos_pub;
+
+int main(int argc, char*argv[])
+{	
+	pos_pub = n.advertise<geometry_msgs::Point>("puck_pos", 1000);
+
+	geometry_msgs::Point point;
+	point.x = 0 ; 
+	point.y = 0.25 ;
+	
+	for (int t = 0 ; t < 30 ; t++)
+	{
+		point.x += 0.25 * (t/30.0);
+		point.y -= 0.125 * (t/30.0);
+		pos_pub.publish(point); 
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000/30.0));
+	}
+	
+	for (int t = 0 ; t < 30 ; t++)
+	{
+		point.x -= 0.25 * (t/30.0);
+		point.y -= 0.125 * (t/30.0);
+		pos_pub.publish(point); 
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000/30.0));
+	}
+	
+}
