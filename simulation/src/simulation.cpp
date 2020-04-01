54d9c637854259bc9827c9989af78900f44bd015
EDesbiens <etiennedesbiens@hotmail.com>
2020-03-31 20:14:12 -0400

    modification in simulation

--- simulation/src/simulation.cpp
+++ simulation/src/simulation.cpp
@@ -20,30 +20,39 @@ int main(int argc, char*argv[])
 	puck_pos_pub.publish(point); 
 	
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	point.x = 0.125; 
+	/*point.y = 0.125; 
 	puck_pos_pub.publish(point); 
 	
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	point.x = 0.0; 
-	puck_pos_pub.publish(point);
-	/*
-	for (int t = 0 ; t < 30 ; t++)
+	point.y = 0.0; 
+	puck_pos_pub.publish(point);*/
+	
+	for (int t = 0 ; t < 20 ; t++)
 	{
 		point.x += 0.25 * (1/30.0);
-		point.y -= 0.125 * (1/30.0);
+		point.y -= 0.0 * (1/30.0);
 		puck_pos_pub.publish(point); 
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000/30));
 	}
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	
-	for (int t = 0 ; t < 30 ; t++)
+	for (int t = 0 ; t < 20 ; t++)
 	{
 		point.x -= 0.25 * (1/30.0);
-		point.y -= 0.125 * (1/30.0);
+		point.y -= 0.0 * (1/30.0);
 		puck_pos_pub.publish(point); 
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000/30));
-	}*/
+	}
+
+	for (int t = 0 ; t < 10 ; t++)
+	{
+		point.x += 0.25 * (1/30.0);
+		point.y -= 0.0 * (1/30.0);
+		puck_pos_pub.publish(point); 
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000/30));
+	}
+
+	
 	
 	ros::spin();
 	
