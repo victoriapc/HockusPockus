70b429a7da7ca54e6ee3a47b88fc67b9331469a5
William Babin-Demers <babw2501@usherbrooke.ca>
2020-04-09 13:14:41 -0400

    Added Ui vision reconfigure publisher

--- ui/src/js/webcam.js
+++ ui/src/js/webcam.js
@@ -18,8 +18,57 @@ var v_text = null;
 // Apply
 var apply = null;
 
-// Varaibles
+// Variables
 var config_started = false;
+
+// Connecting to ROS
+
+var ros = new ROSLIB.Ros({
+    url : 'ws://localhost:9090'
+});
+
+ros.on('connection', function() {
+    console.log('Connected to websocket server.');
+});
+
+ros.on('error', function(error) {
+    console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+    console.log('Connection to websocket server closed.');
+});
+
+// ----- Publishers -----
+function createPublisher(topicName, msgType) {
+    var pub = new ROSLIB.Topic({
+        ros : ros,
+        name : topicName,
+        messageType : msgType
+    });
+    return pub;
+}
+
+var start_pub = createPublisher('/vision/reconfigure/start', 'std_msgs/Bool');
+var radius_pub = createPublisher('/vision/reconfigure/radius', 'std_msgs/Int32');
+var h_pub = createPublisher('/vision/reconfigure/h', 'std_msgs/Int32');
+var s_pub = createPublisher('/vision/reconfigure/s', 'std_msgs/Int32');
+var v_pub = createPublisher('/vision/reconfigure/v', 'std_msgs/Int32');
+var apply_pub = createPublisher('/vision/reconfigure/apply', 'std_msgs/Bool');
+
+function createBoolMsg(val) {
+    var msg = new ROSLIB.Message({
+        data: val
+    })
+    return msg;
+}
+function createInt32Msg(val) {
+    var msg = new ROSLIB.Message({
+        data : Number(val)
+    });
+    console.log(msg);
+    return msg;
+}
 
 // Once the DOM is loaded
 document.addEventListener("DOMContentLoaded", function(){
@@ -46,7 +95,6 @@ function loadSection() {
 // When window is closing
 window.onbeforeunload = function(){
     if(config_started) {
-        console.log("Conditon true.")
         return "Are you sure?";
     }
 };
@@ -60,6 +108,9 @@ function step_1() {
 };
 
 function step_2() {
+    var start_msg = createBoolMsg(true);
+    start_pub.publish(start_msg);
+
     config_started = true;
     start.addClass("disabled");
     radius.removeClass("disabled");
@@ -71,6 +122,9 @@ function step_3() {
 }
 
 function step_4() {
+    var apply_msg = createBoolMsg(true);
+    apply_pub.publish(apply_msg);
+
     hsv.addClass("disabled");
     apply.removeClass("disabled");
     config_started = false;
@@ -87,14 +141,22 @@ function loadSlider() {
 // Update the current slider value (each time you drag the slider handle)
 function updateR() {
     radius_text.innerHTML = radius_slider.value;
+    var radius_msg = createInt32Msg(radius_slider.value);
+    radius_pub.publish(radius_msg);
 }
 
 function updateH() {
     h_text.innerHTML = h_slider.value;
+    var h_msg = createInt32Msg(h_slider.value);
+    h_pub.publish(h_msg);
 }
 function updateS() {
     s_text.innerHTML = s_slider.value;
+    var s_msg = createInt32Msg(s_slider.value);
+    s_pub.publish(s_msg);
 }
 function updateV() {
     v_text.innerHTML = v_slider.value;
+    var v_msg = createInt32Msg(v_slider.value);
+    v_pub.publish(v_msg);
 }
