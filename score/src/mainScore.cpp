14a36d77f181941f010292152ff3be197ca4abd5
FrancisCardinal <francis.cardinal.01@gmail.com>
2020-04-11 13:51:43 -0400

    changed mainScore so that the NewGameListener class now supports dynamic_reconfigure

--- score/src/mainScore.cpp
+++ score/src/mainScore.cpp
@@ -2,20 +2,16 @@
 #include "ROS_topicNames.h"
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
-#include <std_msgs/Int32.h>
-#include <std_msgs/String.h>
+#include <iostream>
+#include <sstream>
 
 #include <dynamic_reconfigure/server.h>
 #include <score/scoreConfig.h>
 
 ros::Subscriber _startSubscriber;
-ros::Subscriber _nameSubscriber;
-ros::Subscriber _scoreSubscriber;
 
 ros::Publisher _scorePublisher;
 ros::Publisher _endOfGamePublisher ;
-
-void param_callback(score::scoreConfig &cfg, uint32_t level);
 
 class NewGameListener
 {
@@ -40,33 +36,39 @@ public:
 		}
 	}
 	
-	//! callback to refresh the players' names, this is triggered when a message is publish on the ROS_topicNames::PLAYERS_NAMES topic 
+	//! method to refresh the players' names, this is triggered when a message is publish on the ROS_topicNames::PLAYERS_NAMES topic 
     /*!
-      \param i_msg :std_msgs::String that indicates the names of the players
+      \param i_playerNames : std::string that indicates the names of the players, elements are separated by ";"
     */
-	void callbackPlayersNames(const std_msgs::String::ConstPtr& i_msg)
+	void updatePlayersNames(const std::string& i_playerNames)
 	{
-		std::string names = i_msg->data;
-		std::stringstream ss(names);
-		std::string name;
-
-		if (!names.empty())
+		if (!i_playerNames.empty())
 		{
-			while(std::getline(ss,name,'\n'))
+			std::string buffer;                 
+			std::istringstream ss(i_playerNames);       
+
+			m_playerNames.clear();
+
+			while (std::getline(ss, buffer, ';'))
 			{
-				m_playerNames.push_back(name);
+				m_playerNames.push_back(buffer);
 			}
 		}
-
 	}
 
-	//! callback to refresh the score to win, this is triggered when a message is publish on the ROS_topicNames::SCORE_TO_WIN topic 
+	//! method to refresh the score to win, this is triggered when a message is publish on the ROS_topicNames::SCORE_TO_WIN topic 
     /*!
-      \param i_msg :std_msgs::Int32 that indicates the new score to win 
+      \param i_scoreToWin :int that indicates the new score to win 
     */	
-	void callbackScoreToWin(const std_msgs::Int32::ConstPtr& i_msg)
+	void updateScoreToWin(int i_scoreToWin)
 	{
-		m_scoreToWin = i_msg->data;
+		m_scoreToWin = i_scoreToWin;
+	}
+	
+	void param_callback(score::scoreConfig &config, uint32_t level) 
+	{
+		updateScoreToWin(config.goal_limit);
+		updatePlayersNames(config.name_players);
 	}
 	
 private: 
@@ -82,14 +84,12 @@ int main(int argc, char*argv[])
 	dynamic_reconfigure::Server<score::scoreConfig> server;
 	dynamic_reconfigure::Server<score::scoreConfig>::CallbackType f;
 
-	f = boost::bind(&param_callback, _1, _2);
+	f = boost::bind(&NewGameListener::param_callback, _1, _2);
 	server.setCallback(f);
 
 	ros::NodeHandle n;
 	NewGameListener newGameListener = NewGameListener();
 	_startSubscriber = n.subscribe(ROS_topicNames::GAME_STATE, 1000, &NewGameListener::callbackStartGame, &newGameListener);
-	_nameSubscriber = n.subscribe(ROS_topicNames::PLAYERS_NAMES, 1000, &NewGameListener::callbackPlayersNames, &newGameListener);
-	_scoreSubscriber = n.subscribe(ROS_topicNames::SCORE_TO_WIN, 1000, &NewGameListener::callbackScoreToWin, &newGameListener);
 	
 	_scorePublisher = n.advertise<std_msgs::String>(ROS_topicNames::SCORES, 1000);
 	_endOfGamePublisher = n.advertise<std_msgs::Bool>(ROS_topicNames::GAME_STATE, 1000);
@@ -97,13 +97,4 @@ int main(int argc, char*argv[])
 
 
 	ros::spin();
-}
-
-void param_callback(score::scoreConfig &config, uint32_t level) {
-  ROS_INFO("Reconfigure Request: %d %d %s %s %s %s", 
-  	config.player_limit, config.goal_limit,
-	config.name_player_1.c_str(),
-	config.name_player_2.c_str(),
-	config.name_player_3.c_str(),
-	config.name_player_4.c_str());
 }
